a2e3358039d1e23ef76191da5499971270e21f41
Ivan Plenty <ivanplenty@gmail.com>
2013-12-08 13:15:39 -0800

    Added default appender and appender selection code that defers requires

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 module.exports = require('./lib/logger.js');
+
+module.exports.getAppender = function(name){
+	if(name == 'color-console')
+		return require('./lib/appender/color-console.js').ColorConsoleAppender;
+	else if (name == 'console')
+		return require('./lib/appender/console.js').ConsoleAppender;
+	else
+		return require('./lib/appender/color-console.js').ColorConsoleAppender;
+}
+
+module.exports.getDefaultAppender = function(){
+	return new (module.exports.getAppender('color-console'));
+}
